8bd2656f35e800fc4a9b96ef32eb9deac960cf7c
bmccombs369 <bmccombs369@gmail.com>
2019-09-17 13:02:21 -0400

    added more stage creating functions

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -1,4 +1,8 @@
 import React from 'react';
+
+import { createStage } from '../gameHelpers';
+
+// Components
 import Stage from './Stage';
 import Display from './Display';
 import StartButton from './StartButton';
@@ -6,7 +10,7 @@ import StartButton from './StartButton';
 const Tetris = () => {
   return(
     <div>
-      <Stage />
+      <Stage stage={createStage()}/>
       <aside>
         <div>
           <Display text="Score" />
